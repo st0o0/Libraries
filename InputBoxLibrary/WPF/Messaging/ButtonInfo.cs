3a832ef515e0da4ee60f68c37c9cc985ea90bbc8
st0o0 <jan.schloots@bluewin.ch>
2021-11-02 19:23:00 +0100

    add InputBoxLibrary

---
+++ InputBoxLibrary/WPF/Messaging/ButtonInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace InputBoxLibrary.WPF.Messaging
+{
+    internal struct ButtonInfo
+    {
+        internal ButtonInfo(string buttonName, RoutedEventHandler eventHandler)
+        {
+            ButtonEventAction = eventHandler;
+            ButtonName = buttonName;
+        }
+
+        internal RoutedEventHandler ButtonEventAction { get; private set; }
+
+        internal string ButtonName { get; private set; }
+    }
+}
