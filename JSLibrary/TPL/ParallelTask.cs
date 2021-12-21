34c3cca9a8c2414c824f3cb8b6a05688cc8e0695
ST00 <jan.schloots@bluewin.ch>
2021-12-21 11:24:48 +0100

    Update TPL

--- JSLibrary/TPL/ParallelTask.cs
+++ JSLibrary/TPL/ParallelTask.cs
@@ -22,26 +22,30 @@ namespace JSLibrary.TPL
 
         public static async Task<IEnumerable<OutputType>> TaskManyAsync<InputType, OutputType>(IEnumerable<InputType> inputs, Func<InputType, OutputType> func, int degreeParallel = 5, CancellationToken cancellationToken = default)
         {
-            ExecutionDataflowBlockOptions edflbo = new() { MaxDegreeOfParallelism = degreeParallel, CancellationToken = cancellationToken };
+            List<OutputType> outputs = new();
+
+            ExecutionDataflowBlockOptions edflbo = new()
+            {
+                MaxDegreeOfParallelism = degreeParallel,
+                CancellationToken = cancellationToken
+            };
 
             TransformBlock<InputType, OutputType> transformBlock = new(func, edflbo);
 
-            inputs.ToList().ForEach(x => transformBlock.Post(x));
+            ActionBlock<OutputType> actionBlock = new(input =>
+            {
+                outputs.Add(input);
+            }, edflbo);
+
+            transformBlock.LinkTo(actionBlock, new() { PropagateCompletion = true, });
+
+            inputs.ToList().ForEach(x => transformBlock.SendAsync(x, cancellationToken));
 
             transformBlock.Complete();
 
-            await transformBlock.Completion;
+            await actionBlock.Completion;
 
-            List<OutputType> outputs = new();
-
-            if (transformBlock.InputCount == transformBlock.OutputCount)
-            {
-                for (int i = 0; i < transformBlock.OutputCount; i++)
-                {
-                    outputs.Add(transformBlock.Receive());
-                }
-            }
-            else
+            if (inputs.Count() != outputs.Count)
             {
                 throw new Exception();
             }
@@ -50,26 +54,26 @@ namespace JSLibrary.TPL
 
         public static async Task<IEnumerable<OutputType>> TaskManyAsync<InputType, OutputType>(IEnumerable<InputType> inputs, Func<InputType, Task<OutputType>> func, int degreeParallel = 5, CancellationToken cancellationToken = default)
         {
+            List<OutputType> outputs = new();
+
             ExecutionDataflowBlockOptions edflbo = new() { MaxDegreeOfParallelism = degreeParallel, CancellationToken = cancellationToken };
 
             TransformBlock<InputType, OutputType> transformBlock = new(func, edflbo);
+
+            ActionBlock<OutputType> actionBlock = new(input =>
+            {
+                outputs.Add(input);
+            }, edflbo);
+
+            transformBlock.LinkTo(actionBlock, new() { PropagateCompletion = true });
 
             inputs.ToList().ForEach(x => transformBlock.SendAsync(x, cancellationToken));
 
             transformBlock.Complete();
 
-            await transformBlock.Completion;
+            await actionBlock.Completion;
 
-            List<OutputType> outputs = new();
-
-            if (transformBlock.InputCount == transformBlock.OutputCount)
-            {
-                for (int i = 0; i < transformBlock.OutputCount; i++)
-                {
-                    outputs.Add(transformBlock.Receive());
-                }
-            }
-            else
+            if (inputs.Count() != outputs.Count)
             {
                 throw new Exception();
             }
@@ -78,65 +82,67 @@ namespace JSLibrary.TPL
 
         public static async Task<IEnumerable<OutputType>> TaskManyAsync<InputType, MiddleType, OutputType>(IEnumerable<InputType> inputs, Func<InputType, IEnumerable<MiddleType>> func, Func<MiddleType, OutputType> func1, int degreeParallel = 5, CancellationToken cancellationToken = default)
         {
+            List<OutputType> outputs = new();
+
             ExecutionDataflowBlockOptions edflbo = new() { MaxDegreeOfParallelism = degreeParallel, CancellationToken = cancellationToken };
 
             TransformManyBlock<InputType, MiddleType> transformManyBlock = new(func, edflbo);
 
             TransformBlock<MiddleType, OutputType> transformBlock = new(func1, edflbo);
 
+            ActionBlock<OutputType> actionBlock = new(input =>
+            {
+                outputs.Add(input);
+            }, edflbo);
+
             transformManyBlock.LinkTo(transformBlock, new() { PropagateCompletion = true });
+
+            transformBlock.LinkTo(actionBlock, new() { PropagateCompletion = true });
 
             inputs.ToList().ForEach(x => transformManyBlock.SendAsync(x, cancellationToken));
 
             transformManyBlock.Complete();
 
-            await transformBlock.Completion;
+            await actionBlock.Completion;
 
-            List<OutputType> outputs = new();
-
-            if (transformBlock.InputCount == transformBlock.OutputCount)
-            {
-                for (int i = 0; i < transformBlock.OutputCount; i++)
-                {
-                    outputs.Add(transformBlock.Receive(cancellationToken));
-                }
-            }
-            else
+            if (inputs.Count() != outputs.Count)
             {
                 throw new Exception();
             }
+
             return outputs;
         }
 
         public static async Task<IEnumerable<OutputType>> TaskManyAsync<InputType, MiddleType, OutputType>(IEnumerable<InputType> inputs, Func<InputType, Task<IEnumerable<MiddleType>>> func, Func<MiddleType, OutputType> func1, int degreeParallel = 5, CancellationToken cancellationToken = default)
         {
+            List<OutputType> outputs = new();
+
             ExecutionDataflowBlockOptions edflbo = new() { MaxDegreeOfParallelism = degreeParallel, CancellationToken = cancellationToken };
 
             TransformManyBlock<InputType, MiddleType> transformManyBlock = new(func, edflbo);
 
             TransformBlock<MiddleType, OutputType> transformBlock = new(func1, edflbo);
 
+            ActionBlock<OutputType> actionBlock = new(input =>
+            {
+                outputs.Add(input);
+            }, edflbo);
+
             transformManyBlock.LinkTo(transformBlock, new() { PropagateCompletion = true });
+
+            transformBlock.LinkTo(actionBlock, new() { PropagateCompletion = true });
 
             inputs.ToList().ForEach(x => transformManyBlock.SendAsync(x, cancellationToken));
 
             transformManyBlock.Complete();
 
-            await transformBlock.Completion;
+            await actionBlock.Completion;
 
-            List<OutputType> outputs = new();
-
-            if (transformBlock.InputCount == transformBlock.OutputCount)
-            {
-                for (int i = 0; i < transformBlock.OutputCount; i++)
-                {
-                    outputs.Add(transformBlock.Receive(cancellationToken));
-                }
-            }
-            else
+            if (inputs.Count() != outputs.Count)
             {
                 throw new Exception();
             }
+
             return outputs;
         }
     }
