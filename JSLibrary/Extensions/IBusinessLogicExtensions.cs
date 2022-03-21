53e4e45a00aa6377c3d78b6280ad3a919bc479ab
ST00 <jan.schloots@bluewin.ch>
2022-03-21 13:42:50 +0100

    update

--- JSLibrary/Extensions/IBusinessLogicExtensions.cs
+++ JSLibrary/Extensions/IBusinessLogicExtensions.cs
@@ -18,9 +18,22 @@ namespace JSLibrary.Extensions
             }
         }
 
+        public static void AddMany<ModelType, DBContextType>(this IBusinessLogicBase<ModelType, DBContextType> businessLogic, IEnumerable<ModelType> items) where DBContextType : DbContext where ModelType : class, IDBModel
+        {
+            foreach (ModelType model in items)
+            {
+                businessLogic.Add(model);
+            }
+        }
+
         public static async Task<IEnumerable<ModelType>> GetManyAsync<ModelType, DBContextType>(this IBusinessLogicBase<ModelType, DBContextType> businessLogic, IEnumerable<int> ids, CancellationToken cancellationToken = default) where DBContextType : DbContext where ModelType : class, IDBModel
         {
             return (await businessLogic.LoadAsync(cancellationToken)).Where(x => ids.Any(z => z == x.Id)).ToList();
+        }
+
+        public static IEnumerable<ModelType> GetMany<ModelType, DBContextType>(this IBusinessLogicBase<ModelType, DBContextType> businessLogic, IEnumerable<int> ids) where DBContextType : DbContext where ModelType : class, IDBModel
+        {
+            return businessLogic.Load().Where(x => ids.Any(z => z == x.Id)).ToList();
         }
 
         public static async Task UpdateManyAsync<ModelType, DBContextType>(this IBusinessLogicBase<ModelType, DBContextType> businessLogic, IEnumerable<ModelType> items, CancellationToken cancellationToken = default) where DBContextType : DbContext where ModelType : class, IDBModel
@@ -31,11 +44,27 @@ namespace JSLibrary.Extensions
             }
         }
 
+        public static void UpdateMany<ModelType, DBContextType>(this IBusinessLogicBase<ModelType, DBContextType> businessLogic, IEnumerable<ModelType> items) where DBContextType : DbContext where ModelType : class, IDBModel
+        {
+            foreach (ModelType model in items)
+            {
+                businessLogic.Update(model);
+            }
+        }
+
         public static async Task DeleteManyAsync<ModelType, DBContextType>(this IBusinessLogicBase<ModelType, DBContextType> businessLogic, IEnumerable<ModelType> items, CancellationToken cancellationToken = default) where DBContextType : DbContext where ModelType : class, IDBModel
         {
             foreach (ModelType model in items)
             {
                 await businessLogic.DeleteAsync(model, cancellationToken);
+            }
+        }
+
+        public static void DeleteMany<ModelType, DBContextType>(this IBusinessLogicBase<ModelType, DBContextType> businessLogic, IEnumerable<ModelType> items) where DBContextType : DbContext where ModelType : class, IDBModel
+        {
+            foreach (ModelType model in items)
+            {
+                businessLogic.Delete(model);
             }
         }
     }
