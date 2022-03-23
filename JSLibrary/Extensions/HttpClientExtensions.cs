d45201d16351b1d5c587a2933e491c5f79242eee
ST00 <jan.schloots@bluewin.ch>
2022-03-23 19:56:49 +0100

    Update

--- JSLibrary/Extensions/HttpClientExtensions.cs
+++ JSLibrary/Extensions/HttpClientExtensions.cs
@@ -60,7 +60,6 @@ namespace JSLibrary.Extensions
         }
 
         // utf8json Serialize
-
         private static HttpContent Serialize<TValue>(TValue data) => new StringContent(JsonSerializer.ToJsonString(data), Encoding.UTF8, "application/json");
     }
 }
