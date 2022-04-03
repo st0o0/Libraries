8f0a0672d9a55a5b208f6d9d35e03b095c634141
st0o0 <jan.schloots@bluewin.ch>
2022-04-03 18:59:36 +0200

    Update

---
+++ WPFLibrary/Input/AsyncRelayCommand_T.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFLibrary.Input
+{
+    public class AsyncRelayCommand<T> : IAsyncRelayCommand<T>
+    {
+        public Task? ExecutionTask => throw new NotImplementedException();
+
+        public bool CanBeCanceled => throw new NotImplementedException();
+
+        public bool IsCancellationRequested => throw new NotImplementedException();
+
+        public bool IsRunning => throw new NotImplementedException();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public event EventHandler? CanExecuteChanged;
+
+        public void Cancel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanExecute(T? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(T? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ExecuteAsync(T? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ExecuteAsync(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotifyCanExecuteChanged()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
