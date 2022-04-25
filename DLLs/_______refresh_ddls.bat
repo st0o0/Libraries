6b5a17a696df09800acfd0286c3b6bb48466c5e1
st0o0 <jan.schloots@bluewin.ch>
2022-04-25 21:24:25 +0200

    update

--- DLLs/_______refresh_ddls.bat
+++ DLLs/_______refresh_ddls.bat
@@ -1,5 +1,5 @@
 @echo off
 
-xcopy /y "..\InputBoxLibrary\bin\Release\net6.0-windows\InputBoxLibrary.dll"
-xcopy /y "..\JSLibrary\bin\Release\net6.0\JSLibrary.dll"
-xcopy /y "..\WPFLibrary\bin\Release\net6.0-windows\WPFLibrary.dll"
+xcopy /y "..\InputBoxLibrary\bin\debug\net6.0-windows\InputBoxLibrary.dll"
+xcopy /y "..\JSLibrary\bin\Debug\net6.0\JSLibrary.dll"
+xcopy /y "..\WPFLibrary\bin\debug\net6.0-windows\WPFLibrary.dll"
